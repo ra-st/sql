a704451f4bb5bed69dd2f83eafad2b664d99ce3e
saji <104052076+sajist@users.noreply.github.com>
2022-05-10 23:29:09 +0430

    UPDATE FACTORS2

--- shop.sql
+++ shop.sql
@@ -275,16 +275,16 @@ INSERT INTO categories (category_id, name, parent, created_at, created_by)
            (7,'food',null,now(),'+989363236021');
            
  INSERT INTO factors (factor_id, wallet, mode, title, details, amount, data_time, shop)
-    VALUES (1,'25','pay_in','خرید گوشی','fast post','1','22/1/2022','فروشگاه کوروش'),
-           (2,'30','pay_in','خرید کیف','Breakable','1','20/1/2022','فروشگاه دی جی کالا'),
-           (3,'10','default','lap tap','electrical thing','3','31/1/2021','ebuy'),
-           (4,'5','pay_out','tv','sensitive','5','25/3/2022','amazun'),
-           (5,'8','pay_out','car','new car','1','18/8/2020','divar'),
-           (6,'15','pay_in','glasses','sensitive','12','20/3/2021','shop glass'),
-           (7,'30','pay_in','shoe','black shoe','53','15/6/2019','bella shoe'),
-           (8,'25','pay_out','bag','Leather bag','100','15/8/2017','dior shop'),
-           (9,'60','pay_in','phone','expensive','500','3/6/2015','mobie.ir'),
-           (10,'80','pay_out','drog','in time recive','30','21/5/2020','pharmacy');
+    VALUES (1,'1','pay_in','خرید گوشی','fast post','1','22/1/2022','1'),
+           (2,'3','pay_in','خرید کیف','Breakable','1','20/1/2022','2'),
+           (3,'5','default','lap tap','electrical thing','3','31/1/2021','e3'),
+           (4,'5','pay_out','tv','sensitive','5','25/3/2022','3'),
+           (5,'4','pay_out','car','new car','1','18/8/2020','2'),
+           (6,'1','pay_in','glasses','sensitive','12','20/3/2021','3'),
+           (7,'3','pay_in','shoe','black shoe','53','15/6/2019','2'),
+           (8,'2','pay_out','bag','Leather bag','100','15/8/2017','1'),
+           (9,'1','pay_in','phone','expensive','500','3/6/2015','2'),
+           (10,'2','pay_out','drog','in time recive','30','21/5/2020','1');
            
 
 
